a067b4a92caf204352e4830d54de20937ff694af
Klaudiusz Skowronski <claude-plos@o2.pl>
2016-11-17 14:30:37 +0100

    interface front-end

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
-angular.module('ngdemo', ['ngdemo.filters', 'ngdemo.services', 'ngdemo.directives', 'ngdemo.controllers']).
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/user-list', {templateUrl: 'partials/user-list.html', controller: 'UserListCtrl'});
-        $routeProvider.when('/user-detail/:id', {templateUrl: 'partials/user-detail.html', controller: 'UserDetailCtrl'});
-        $routeProvider.when('/user-creation', {templateUrl: 'partials/user-creation.html', controller: 'UserCreationCtrl'});
-        $routeProvider.otherwise({redirectTo: '/user-list'});
-    }]);
+'use strict';
+
+//angular.module('ksApp', ['ngRoute']);
+// Declare app level module which depends on filters, and services
+angular.module('ksApp', ['ngRoute','ksApp.services', 'ksApp.controllers']).
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/FZPimport', {templateUrl: 'partials/FZPimport.html'});
+    $routeProvider.when('/user-list', {templateUrl: 'partials/user-list.html', controller: 'UserListCtrl'});
+    $routeProvider.when('/user-detail/:id', {templateUrl: 'partials/user-detail.html', controller: 'UserDetailCtrl'});
+    $routeProvider.when('/user-creation', {templateUrl: 'partials/user-creation.html', controller: 'UserCreationCtrl'});
+    $routeProvider.otherwise({redirectTo: '/'});
+}]);
+
+
+
+
--- js/services.js
+++ js/services.js
@@ -1,14 +1,17 @@
-var services = angular.module('ngdemo.services', ['ngResource']);
+
+/* Services */
+
+var services = angular.module('ksApp.services', ['ngResource']);
 
 services.factory('UsersFactory', function ($resource) {
-    return $resource('/ngdemo/web/users', {}, {
+    return $resource(' http://localhost:port/napFzpStatOrlBP_Rest/napFZP/napuzytkownik', {port: ':40884'}, {
         query: { method: 'GET', isArray: true },
         create: { method: 'POST' }
     })
 });
 
 services.factory('UserFactory', function ($resource) {
-    return $resource('/ngdemo/web/users/:id', {}, {
+    return $resource('/napFzpStatOrlBP_Rest/napFZP/napuzytkownik/:id', {}, {
         show: { method: 'GET' },
         update: { method: 'PUT', params: {id: '@id'} },
         delete: { method: 'DELETE', params: {id: '@id'} }
